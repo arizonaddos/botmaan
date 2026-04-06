75e427ba43bd0a986034c7d59034ddf5eda6cc73
arizonaddos <125118475+arizonaddos@users.noreply.github.com>
2026-04-06 20:02:46 +0700

    Modify admin ID and add required ID in config
    Updated ADMIN_IDS and added REQUIRED_ID for link.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_TOKEN = "8664455569:AAFXpOprkDPFN_HqsSqrTyv95fAE3KPp_y4"  # Token từ @BotFather
-ADMIN_IDS = [8484871348]  # ID Telegram của admin chính @thienlam6868
+ADMIN_IDS = [5088042581]  # ID Telegram của admin chính @binjet
 DATABASE = "bot_data.db"
 KEY_EXPIRY = {"1n": 86400, "1t": 2592000, "1y": 31536000}  # Thời gian hết hạn theo giây
 RATE_LIMIT = 5  # Số lệnh tối đa trong 10 giây
